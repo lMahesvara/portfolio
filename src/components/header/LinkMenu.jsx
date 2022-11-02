6666d69e108ec43f2a6493f2eecc8139794f20ea
lMahesvara <erick082016@gmail.com>
2022-11-01 17:30:22 -0700

    fix: change text-color to black

--- src/components/header/LinkMenu.jsx
+++ src/components/header/LinkMenu.jsx
@@ -3,13 +3,13 @@ import { Link } from 'react-scroll'
 const LinkMenu = ({ children, section, offset, onClick }) => {
   return (
     <Link
-      activeClass='md:border-b-2 md:border-primary-500 text-primary-500 md:text-white'
+      activeClass='md:border-b-2 md:border-primary-500 !text-primary-500 md:!text-white'
       to={section}
       spy={true}
       smooth={true}
       offset={offset}
       duration={400}
-      className='w-full cursor-pointer p-5 text-center  transition-colors duration-300 ease-in-out hover:text-primary-500 md:w-auto md:p-0'
+      className='w-full cursor-pointer p-5 text-center text-white  transition-colors duration-300 ease-in-out hover:text-primary-500 md:w-auto md:p-0'
       onClick={onClick}
     >
       {children}
