47ca6c251b1302704695d06c6a8319feb6448bb3
lMahesvara <erick082016@gmail.com>
2022-11-02 18:38:18 -0700

    feat: show more button on projects component

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -10,14 +10,28 @@ const ProjectCard = ({
   tags,
   status
 }) => {
+  const renderTags = () => {
+    return tags.map((tag, index) => (
+      <span
+        key={index}
+        className='rounded-full bg-primary-300 px-2 py-1 text-xs font-semibold text-black'
+      >
+        {tag}
+      </span>
+    ))
+  }
+  const imageStyle = image
+    ? 'opacity-0 hover:opacity-100'
+    : 'opacity-100 hover:opacity-0'
+
   return (
-    <article className='flex h-96 w-80 flex-col items-start justify-start rounded-md bg-white'>
+    <article className='relative flex min-h-[24rem] w-80 flex-col items-start justify-start rounded-md bg-white'>
       <h3 className='w-full rounded-t-md bg-primary-500 p-2 text-center font-inter text-lg font-bold text-black'>
         {title}
       </h3>
       <div className='relative h-40 min-h-[10rem] w-80 cursor-pointer overflow-hidden'>
         <img
-          src={`src/images/${image}`}
+          src={`src/images/${image || 'default.svg'}`}
           alt={title}
           className='h-full w-full'
         />
@@ -26,20 +40,25 @@ const ProjectCard = ({
             target='_blank'
             rel='noreferrer noopener'
             href={demo}
-            className='absolute top-0 flex h-full w-full items-center justify-center bg-black/80 text-xl text-white opacity-0 transition duration-300 ease-in-out hover:opacity-100'
+            className={`absolute top-0 flex h-full w-full items-center justify-center bg-black/80 text-xl text-white ${imageStyle} transition duration-300 ease-in-out `}
           >
             <span>Deploy</span>
             <BiLinkExternal className='ml-2 h-6 w-6' />
           </a>
         ) : (
-          <div className='absolute top-0 flex h-full w-full items-center justify-center bg-black/80 text-xl text-white opacity-0 transition duration-300 ease-in-out hover:opacity-100'>
+          <div
+            className={`${imageStyle} absolute top-0 flex h-full w-full items-center justify-center bg-black/80 text-xl text-white transition duration-300 ease-in-out`}
+          >
             <span>{status ?? 'No demo available'}</span>
           </div>
         )}
       </div>
       <p className='h-24 w-full p-5 text-sm'>{description}</p>
-      <p className='w-full pl-5 font-inter text-xs '>{tags.join(' - ')}</p>
-      <div className='flex w-full items-center justify-end gap-2 p-5'>
+      {/* <p className='w-full pl-5 font-inter text-xs '>{tags.join(' - ')}</p> */}
+      <div className='flex flex-wrap items-center gap-2 px-5'>
+        {renderTags()}
+      </div>
+      <div className='absolute bottom-5 right-5  flex items-center justify-end gap-2'>
         {demo && (
           <a target='_blank' rel='noreferrer noopener' href={demo}>
             <BiLinkExternal className=' h-6 w-6 text-black transition-all duration-300  hover:text-primary-700' />
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,20 +1,49 @@
 import ProjectCard from './ProjectCard'
 import SectionTitle from './SectionTitle'
 import data from '../data/data.json'
+import { useState, useEffect } from 'react'
 
 const { projects } = data
 
 const Projects = () => {
+  const [total, setTotal] = useState(false)
+  const [projectsToShow, setProjectsToShow] = useState([projects])
+  const handleShowMore = () => {
+    setTotal(!total)
+  }
+  useEffect(() => {
+    const getWindowsWidth = () => {
+      if (total) setProjectsToShow(projects)
+      else if (window.innerWidth < 768) {
+        setProjectsToShow(projects.slice(0, 3))
+      } else {
+        setProjectsToShow(projects.slice(0, 6))
+      }
+    }
+    window.addEventListener('resize', getWindowsWidth)
+    getWindowsWidth()
+    return () => window.removeEventListener('resize', getWindowsWidth)
+  }, [total])
+  const buttonText = total ? 'Show Less' : 'Show More'
+
   return (
     <section
       id='projects'
       className='mx-auto mt-8 mb-4 flex flex-col items-center gap-16'
     >
       <SectionTitle>Projects</SectionTitle>
-      <div className='grid grid-cols-1 gap-10  md:grid-cols-2 lg:grid-cols-3'>
-        {projects.map((project, index) => (
-          <ProjectCard key={index} {...project} />
-        ))}
+      <div className='flex flex-col items-center gap-4'>
+        <div className='grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3'>
+          {projectsToShow.map((project, index) => (
+            <ProjectCard key={index} {...project} />
+          ))}
+        </div>
+        <button
+          className='rounded-md bg-primary-500 px-4 py-2 text-white shadow-md transition-colors duration-300 ease-in-out hover:bg-primary-600'
+          onClick={handleShowMore}
+        >
+          {buttonText}
+        </button>
       </div>
     </section>
   )
